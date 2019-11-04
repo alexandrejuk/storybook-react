0c8edd86487e54162388dafdd181af3bd2fff5d2
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2019-11-04 18:19:10 -0300

    add new features

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import CartIcon from '../../assets/icons/cart.svg'
+import UserIcon from '../../assets/icons/user.svg'
 import './style.css'
 
 const NavBar = ({
@@ -18,32 +19,41 @@ const NavBar = ({
             />
           </div>
         </Link>
-        <Link to="/checkout">
-          <div
-            className="cardIconWrapper"
-            >
-            <img
-              className="cardIcon"
-              src={CartIcon}
-              alt="shopping cart"
-              />
-            <span
-              className="cartTotal"
+       <div className="actionNavBar">
+        <Link to="login">
+            <div className="loginSingUp">
+              <img className="loginIcon" src={UserIcon} alt="icon login"/>
+              <h5>Entre ou <br/> cadastre-se</h5>
+            </div>
+          </Link>
+          <Link to="/checkout">
+            <div
+              className="cardIconWrapper"
               >
-              {
-                cartItems > 9 ? '+9' : cartItems
-              }
-            </span>
-          </div>
-        </Link>
+              <img
+                className="cardIcon"
+                src={CartIcon}
+                alt="shopping cart"
+                />
+              <span
+                className="cartTotal"
+                >
+                {
+                  cartItems > 9 ? '+9' : cartItems
+                }
+              </span>
+            </div>
+          </Link>
+       </div>
       </div>
     </div>
     <div className="navbar navList">
       <div className="nav-wrapper nav-white">
         <ul>
-          <li>Livros</li>
-          <li>Brinquedos</li>
+          <li>Finaceiro</li>
+          <li>Técnologia</li>
           <li>Games</li>
+          <li>Idiomas</li>
         </ul>
       </div>
     </div>
--- src/containers/Products/index.js
+++ src/containers/Products/index.js
@@ -27,6 +27,9 @@ const Products = ({
 
   return (
     <div className="contentProductsMain">
+      <div className="slider">
+        <img src="https://lojasaraiva.vteximg.com.br/arquivos/ids/3446498/1109_tv_niver_saraiva_ebooks.jpg?v=637040025316170000" alt="slider" />
+      </div>
       {map(productItem, products)}
     </div>
   )
