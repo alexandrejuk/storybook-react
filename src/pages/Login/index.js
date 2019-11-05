35eec7aa57d7dd01831e84942074474721084032
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2019-11-04 21:29:27 -0300

    fix

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,10 +3,7 @@ import { withRouter } from 'react-router-dom'
 import { isEmpty } from 'ramda'
 import { connect } from 'react-redux'
 import LoginContainer from '../../containers/Login'
-import {
-  logged,
-  loggout,
-} from '../../components/actions/login'
+import { logged } from '../../components/actions/login'
 const Login = ({
   login,
   loggedUser,
@@ -26,7 +23,7 @@ const Login = ({
 
   const handleSubmit = () => {
     if(!isEmpty(formData)) {
-      console.log(formData)
+      // loggedUser(formData)
       return
     }
   }
